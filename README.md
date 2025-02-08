06194b1baa96cee9d3ddf0aa762109ab2209b565
tinomager <tino.mager@outlook.de>
2025-02-08 20:36:45 +0100

    initial commit with working version using transformers library

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# document-classification
+# Document Classification
+
+This is a simple sample for me to use a zero-shot classification model to classify the content of PDF documents into predefined categories.
+
+## Requirements
+
+To install the required dependencies, run:
+
+```sh
+pip install -r requirements.txt
+```
+
+Be sure that you have installed Pytorch on your system.
+
+## Usage
+1. Place your PDF documents in the docs folder.
+2. Run the zeroshotclassificiation.py script:
+
+```sh
+python zeroshotclassificiation.py
+```
+
+The script will read all PDF files in the docs folder, extract their text, and classify the content into the following categories:
+
+- Sport
+- Politik
+- Wirtschaft
+- Kultur
+- Wissenschaft
+- Technik
+- Gesundheit
+- Reisen
+- Essen
+
+You can adjust the labels accordingly to your use case as needed.
+
+## Script Details
+The main script is zeroshotclassificiation.py. It performs the following steps:
+
+1. Uses the transformers library to load a zero-shot classification model.
+2. Reads all PDF files in the docs folder using pypdf.
+3. Extracts text from each PDF file.
+4. Classifies the extracted text into predefined categories.
+5. Prints the classification results.
+
+## License
+This project is licensed under the MIT License. See the LICENSE file for details.
---
+++ zeroshotclassificiation.py
@@ -0,0 +1,32 @@
+from transformers import pipeline
+from pypdf import PdfReader
+import glob, os
+
+classifier = pipeline("zero-shot-classification", model="MoritzLaurer/mDeBERTa-v3-base-xnli-multilingual-nli-2mil7")
+
+def get_all_files_from_folder(subfolder="docs"):
+    all_texts = []
+    file_names = glob.glob(os.path.join(subfolder, "**", "*.pdf"), recursive=True)
+    for file_name in file_names:
+        print(f"Reading file: {file_name}")
+        reader = PdfReader(file_name)
+        page_texts = [page.extract_text() for page in reader.pages]
+        all_texts.append({ "file_name" : file_name, "text" : " ".join(page_texts)})
+        print("File read successfully")
+    return all_texts
+
+#text = "Eintracht Frankfurt gewinnt die Europa League nach 6:5-Erfolg im Elfmeterschießen gegen die Glasgow Rangers"
+labels = ["Sport", "Politik", "Wirtschaft", "Kultur", "Wissenschaft", "Technik", "Gesundheit", "Reisen", "Essen"]
+
+all_texts = get_all_files_from_folder()
+for entry in all_texts:
+    text = entry["text"]
+    filename = entry["file_name"]
+    result = classifier(text, labels)
+
+    print(f"File: {filename}")
+    print("Klassifikationen:")
+    for label, score in zip(result["labels"], result["scores"]):
+        print(f"{label}: {score:.3f}")
+
+    print("\n-----------------------------------\n")
